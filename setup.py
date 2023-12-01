812107818f4bce7eac8a08fe253f3d2b8a077850
123mjb <micahbutt@outlook.com>
2023-12-01 19:03:28 +0000

    Start

---
+++ setup.py
@@ -0,0 +1,20 @@
+from cx_Freeze import setup, Executable
+
+base = None    
+
+executables = [Executable("game.py", base=base)]
+
+packages = ["idna"]
+options = {
+    'build_exe': {    
+        'packages':packages,
+    },    
+}
+
+setup(
+    name = "<test>",
+    options = options,
+    version = "1.0",
+    description = 'A',
+    executables = executables
+)
