711c036cf522c7939e5056c2c6ee6f0f6e9b0329
123mjb <micahbutt@outlook.com>
2023-12-12 20:43:51 +0000

    new system start

--- game.py
+++ game.py
@@ -3,7 +3,10 @@ from pygame.locals import *
 import pygame.gfxdraw
 import random
 import math
+import json
 
+with open("levels.json","r") as read:
+    lvlsobj = json.load(read)
 pygame.init()
 FPS = 60
 clock = pygame.time.Clock()
@@ -14,6 +17,8 @@ white = (255,255,255)
 class Sprite(pygame.sprite.Sprite):
     def __init__(self, colour,height,width) -> None:
         super().__init__()
+        self.sprint = False
+        self.wj = False
         self.oheight = height
         self.owidth = width
         self.adheight = height
@@ -22,8 +27,8 @@ class Sprite(pygame.sprite.Sprite):
         self.adjustmentw = 1
         self.progression = 0
         self.colour = colour
-        self.rs = [1/16,1/8,1/2,3/4,15/16,7/16,14/16]
-        self.degs = [random.randrange(0,360) for _ in range(0,7)]
+        self.rs = [i/12 for i in range(1,12)]
+        self.degs = [random.randrange(0,360) for _ in range(0,len(self.rs))]
         self.jumping = False
         self.crouching = False
         self.ymoment = 0
@@ -58,15 +63,16 @@ class Sprite(pygame.sprite.Sprite):
         self.adheight = self.owidth * self.adjustmenth
         self.image = pygame.Surface([self.adwidth,self.adheight])
         draw_rect_alpha(self.image,self.image.get_rect())
-        self.progression +=5*direction*-1
+        self.progression +=10*direction*-1
         if self.progression > 359: self.progression = 0
-        pygame.gfxdraw.filled_circle(self.image,int(round(self.adwidth/2)),int(round(self.adheight/2)),int(round(self.adwidth/2)),(self.colour[0]-20 if self.colour[0]>19 else 0,self.colour[1]-20 if self.colour[1]>19 else 0,self.colour[2]-20 if self.colour[2]>19 else 0))
+        adjusted_colour = (255-self.colour[0],255-self.colour[1],255-self.colour[2])
+        pygame.gfxdraw.filled_circle(self.image,int(round(self.adwidth/2)),int(round(self.adheight/2)),int(round(self.adwidth/2)),self.colour)
         pygame.gfxdraw.circle(self.image,int(round(self.adheight/2)),int(round(self.adheight/2)),int(round(self.adwidth/2)),self.colour)
         try: s
         except NameError: s=0
-        if self.progression % 20 == 0: s = random.randrange(0,31)
-        for i in range(0,7):
-            pygame.draw.arc(self.image,self.colour,Rect(int(round((self.adheight-self.adheight*self.rs[i])/2)),int(round((self.adheight-self.adheight*self.rs[i])/2)),int(round(self.adheight*self.rs[i])),int(round(self.adheight*self.rs[i]))),math.radians(self.progression+self.degs[i]),math.radians(self.progression+self.degs[i]+15+s),int(round(self.adwidth/2/15)))      
+        if self.progression % 20 == 0: s = random.randrange(0,5)
+        for i in range(0,len(self.rs)):
+            pygame.draw.arc(self.image,adjusted_colour,Rect(int(round((self.adheight-self.adheight*self.rs[i])/2)),int(round((self.adheight-self.adheight*self.rs[i])/2)),int(round(self.adheight*self.rs[i])),int(round(self.adheight*self.rs[i]))),math.radians(self.progression+self.degs[i]),math.radians(self.progression+self.degs[i]+45+s),int(round(self.adwidth/2/15)))      
     def jump(self):
         self.adheight = self.oheight * self.adjustmenth
         self.adwidth = self.owidth * self.adjustmentw
@@ -93,56 +99,55 @@ class Sprite(pygame.sprite.Sprite):
         pygame.draw.rect(self.image,self.colour,pygame.Rect((self.adwidth-body_width)/2,head_height+shoulder_height+body_height,body_width*7/16,legs_height))
         pygame.draw.rect(self.image,self.colour,pygame.Rect((self.adwidth-body_width)/2+body_width*9/16,head_height+shoulder_height+body_height,body_width*7/16+1,legs_height))
     def move(self):
-        """_ = pygame.sprite.spritecollideany(self,terrain_sprites)
-        if _ is None:
-            self.falling = True
-            self.ymoment -= 1
-        else:
-            if self.rect.top > _.rect.top:
-                pass
-            elif self.rect.bottom < _.rect.bottom:
-                self.rect.bottom = _.rect.top + 1
-                self.ymoment = 0
-                self.falling = False
-            else:
-                self.ymoment = 0
-                self.falling = False"""
-        
         pressed_keys = pygame.key.get_pressed()
+        if pressed_keys[K_LSHIFT]:
+            self.sprint = True
+        else: self.sprint = False
         if pressed_keys[K_w] or pressed_keys[K_SPACE]:
             self.jump()
             self.jumping = True
-            if not self.falling:
-                self.ymoment = 15
+            if not self.falling or self.wj:
+                self.ymoment = 17
         else: self.jumping = False
         if pressed_keys[K_s]:
             self.crouching = True
         if pressed_keys[K_a]:
             if self.rect.left > 0:
-                self.movecheck(-5)
+                self.movecheck(-7 if self.sprint else -5)
                 if not self.falling: self.movesideways(-1)
         if pressed_keys[K_d]:
             if self.rect.right < screen.get_width():
-                self.movecheck(5)
+                self.movecheck(7 if self.sprint else 5)
                 if not self.falling: self.movesideways(1)
         self.rect.move_ip(0,-self.ymoment)
         __ = pygame.sprite.spritecollideany(self,terrain_sprites)
         if not __ is None:
-            self.ymoment = 0
-            self.falling = False
-            self.rect.bottom = __.rect.top
+            if self.rect.top + self.ymoment > __.rect.bottom:
+                self.rect.top = __.rect.bottom
+                self.ymoment = 0
+                self.ymoment -=1 if not self.jumping else 0.75
+                self.falling = True
+            else:
+                self.ymoment = 0
+                self.falling = False
+                self.rect.bottom = __.rect.top
         else: 
-            self.ymoment -=1
+            self.ymoment -=1 if not self.jumping else 0.75
             self.falling = True
+        try: pygame.sprite.spritecollideany(self,portal_sprites).changelevel()
+        except: pass
     def movecheck(self,direction):
         self.rect.move_ip(direction,0)
         __ = pygame.sprite.spritecollideany(self,terrain_sprites)
         if not __ is None:
             if direction>0:
                 self.rect.right = __.rect.left
+                self.wj = True
             else:
                 self.rect.left = __.rect.right
+                self.wj = True
         else: 
+            self.wj = False
             pass
 
 class terrainsprites(pygame.sprite.Sprite):
@@ -152,8 +157,7 @@ class terrainsprites(pygame.sprite.Sprite):
         pygame.draw.rect(self.image,color_,pygame.Rect(0,0,width,height))
         self.rect = self.image.get_rect()
         self.rect.x = coords[0]
-        self.rect.y = coords[1]
-        
+        self.rect.y = coords[1]    
 class portalsprites(pygame.sprite.Sprite):
     def __init__(self, color_,coords, width,height,level) -> None:
         super().__init__()
@@ -174,20 +178,30 @@ def draw_rect_alpha(surface,rect):
 
 class Levels():
     def __init__(self) -> None:
-        pass
-    def reset(self,coords:tuple[int,int]):
+        self.w = (255,255,255)
+        self.p = (255,127.5,255)
+        self.g = (0,255,0)
+        
+        self.pw = screensize[0]/40
+        self.ph = screensize[1]/5
+    def reset(self,coords:tuple[float,float]):
         terrain_sprites.empty()
-        object_.rect.x = coords[0]
-        object_.rect.y = coords[1]
+        portal_sprites.empty()
+        object_.rect.centerx = coords[0]
+        object_.rect.bottom = coords[1]
     def start(self):
-        self.reset((screensize[0]/2,screensize[1]/2))
-        terrain_sprites.add(terrainsprites((255,255,255),(0,screensize[1] - screensize[1]/10),screensize[0],screensize[1]/10))
-        portal_sprites.add(portalsprites((255,255,255),(0,screensize[1] - screensize[1]/5),screensize[0]/20,screensize[1]/5,1))
+        self.reset((screensize[0]/2,screensize[1]*9/10))
+        self.createt(self.w,(0,screensize[1] - screensize[1]/10),screensize[0],screensize[1]/10)
+        self.createp(self.p,(0,screensize[1]*4/5-screensize[1]/10),self.pw,self.ph,1)
+        self.createt(self.w,(screensize[0]/3,screensize[1]*7/10),screensize[0]/3,screensize[1]/20)
     def one(self):
-        self.reset((screensize[0]/2,screensize[1]/2))
-        terrain_sprites.add(terrainsprites((255,255,255),(0,screensize[1] - screensize[1]/10),screensize[0],screensize[1]/10))
-        portal_sprites.add(portalsprites((255,255,255),(0,screensize[1] - screensize[1]/5),screensize[0]/20,screensize[1]/5,0))
-
+        self.reset((screensize[0]/2,screensize[1]*9/10))
+        self.createt(self.w,(0,screensize[1] - screensize[1]/10),screensize[0],screensize[1]/10)
+        self.createp(self.p,(0,screensize[1]*9/10 - self.ph),self.pw,self.ph,0)
+    def createt(self,colour:tuple[int,int,int],coords,length:float,height:float):
+        terrain_sprites.add(terrainsprites(colour,coords,length,height))
+    def createp(self,colour,coords,length,height,lvl):
+        portal_sprites.add(portalsprites(colour,coords,length,height,lvl))
 terrain_sprites = pygame.sprite.Group()
 portal_sprites = pygame.sprite.Group()
 all_sprites_list = pygame.sprite.Group()
@@ -199,12 +213,61 @@ all_sprites_list.add(object_)
 levels = Levels()
 levels.start()
 
+def interpret(string:str):
+    s = string.split("")
+    p = []
+    for i in range(0,len(s)):
+        if s[i] == "s": s[i] = str(screensize[0])
+        if s[i] == "d": s[i] = str(screensize[1])
+    for i in range(0,len(s)):
+        if s[i] == "*": 
+            p = []
+            if i > 1:
+                for j in range(0,i-1):
+                    p.append(s[j])
+            p.append(str(int(s[i-1])*int(s[i+1])))
+            if len(s) - i > 2:
+                for j in range(i+2,len(s) - 1):
+                    p.append(s[j])
+            s=p
+        if s[i] == "/": 
+            p = []
+            if i > 1:
+                for j in range(0,i-1):
+                    p.append(s[j])
+            p.append(str(int(s[i-1])/int(s[i+1])))
+            if len(s) - i > 2:
+                for j in range(i+2,len(s) - 1):
+                    p.append(s[j])
+            s=p
+    for i in range(0,len(s)):
+        if s[i] == "+": 
+            p = []
+            if i > 1:
+                for j in range(0,i-1):
+                    p.append(s[j])
+            p.append(str(int(s[i-1])+int(s[i+1])))
+            if len(s) - i > 2:
+                for j in range(i+2,len(s) - 1):
+                    p.append(s[j])
+            s=p
+        if s[i] == "-": 
+            p = []
+            if i > 1:
+                for j in range(0,i-1):
+                    p.append(s[j])
+            p.append(str(int(s[i-1])-int(s[i+1])))
+            if len(s) - i > 2:
+                for j in range(i+2,len(s) - 1):
+                    p.append(s[j])
+            s=p
+    return s[0]
+        
+
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-    try: pygame.sprite.spritecollideany(object_,portal_sprites).changelevel()
-    except: pass
     object_.move()
     all_sprites_list.update()
     screen.fill((0,0,0))
